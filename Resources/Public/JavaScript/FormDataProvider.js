9aec3e422601ebe8d217474626fd56a2e2830eac
Stig Nørgaard Færch <stigfaerch@gmail.com>
2026-04-16 12:02:57 +0200

    [TASK] Fix JS error

--- Resources/Public/JavaScript/FormDataProvider.js
+++ Resources/Public/JavaScript/FormDataProvider.js
@@ -1,2 +1,2 @@
-document.querySelector('.t3js-editform-new').classList.add('d-none');
-document.querySelector('.t3js-editform-delete-record').classList.add('d-none');
+document.querySelector('.t3js-editform-new')?.classList.add('d-none');
+document.querySelector('.t3js-editform-delete-record')?.classList.add('d-none');
